46751b7303dba924ef93dcee7437cb20417c3175
adittya077 <akuntugaswahadit@gmail.com>
2024-11-03 11:14:28 +0000

    FIKS

--- program.cpp
+++ program.cpp
@@ -7,10 +7,10 @@ void tampilkanBiodata() {
     int umur = 20;
     string alamat = "Alamat Anda";
 
-    cout << "=== Biodata ===" << end1;
-    cout << "Nama   :  "<< nama << end1;
-    cout << "umur   :  "<< umur << " tahun" << end 1 ;
-    cout << "Alamat :  "<< alamat << end1;
+    cout << "=== Biodata ===" << endl;
+    cout << "Nama   :  "<< nama << endl;
+    cout << "umur   :  "<< umur << " tahun" << endl;
+    cout << "Alamat :  "<< alamat << endl;
 }
 
 // Fungsi untuk penjumlahan dua bilangan
@@ -20,39 +20,49 @@ void penjumlahan() {
     cin >> a;
     cout << "Masukkan bilangan kedua : ";
     cin >> b;
-    cout << "Hasil penjumlahan: " << a + b << end1;
+    cout << "Hasil penjumlahan: " << a + b << endl;
 }
 
 // Fungsi untuk menghitung luas segitiga
-void luassegitiga() {
-    const float PI = 3.14159
-    float jarijari;
+void luasSegitiga() {
+    const float PI = 3.14159;
+    float jariJari;
     cout << "\nMasukkan jari-jari lingkaran: ";
-    cin >> jarijari;
-    cout << " Luas lingkaran:  " << PI * jarijari << end1;
+    cin >> jariJari;
+    cout << " Luas lingkaran:  " << PI * jariJari * jariJari << endl;
+}
+
+
+// Fungsi untuk menghitung luas lingkaran
+void luasLingkaran() {
+    const float PI = 3.14159;
+    float jariJari;
+    cout << "\nMasukkan jari-jari lingkaran: ";
+    cin >> jariJari;
+    cout << "Luas lingkaran: " << PI * jariJari * jariJari << endl;
 }
 
 // Fungsi untuk konversi detik ke menit
-void konversiDetikkeMenit() {
+void konversiDetikKeMenit() {
     int detik;
     cout << "\nMasukkan jumlah detik: ";
     cin >> detik;
     int menit = detik / 60;
     int sisaDetik = detik % 60;
-    cout << " Hasil konversi: " << menit << " menit dan " << sisaDetik << " detik" <<end1;
+    cout << " Hasil konversi: " << menit << " menit dan " << sisaDetik << " detik" <<endl;
 }
 
 
 int main() {
     int pilihan;
     do {
-        cout << "\n=== Menu program ===" << end1;
-        cout << "1. Tampilkan Biodata" << end1;
-        cout << "2. penjumlahan Dua Bilangan" << end1;
-        cout << "3. Menghitung Luas Segitiga" << end1;
-        cout << "4. Menghitung Luas Lingkaran" << end1;
-        cout << "5. Konversi Detik Ke Menit"  << end1;
-        cout << "6. Keluar" << end1;
+        cout << "\n=== Menu program ===" << endl;
+        cout << "1. Tampilkan Biodata" << endl;
+        cout << "2. penjumlahan Dua Bilangan" << endl;
+        cout << "3. Menghitung Luas Segitiga" << endl;
+        cout << "4. Menghitung Luas Lingkaran" << endl;
+        cout << "5. Konversi Detik Ke Menit"  << endl;
+        cout << "6. Keluar" << endl;
         cout << "Pilih menu: ";
         cin >> pilihan;
 
@@ -62,9 +72,8 @@ int main() {
             case 3: luasSegitiga(); break;
             case 4: luasLingkaran(); break;
             case 5: konversiDetikKeMenit(); break;
-            case 6: cout << " Keluar dari program." <<  end1; break;
-            default: cout << "Pilihan tidak valid!" << end1;
-
+            case 6: cout << "Keluar dari program." <<  endl; break;
+            default: cout << "Pilihan tidak valid!" << endl;
         }
 
     } while (pilihan !=6);

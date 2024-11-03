deeccdd547b9ebf85fdae04a7700e9d3961177a4
adittya077 <akuntugaswahadit@gmail.com>
2024-11-03 10:53:03 +0000

    baru

--- program.cpp
+++ program.cpp
@@ -40,6 +40,35 @@ void konversiDetikkeMenit() {
     int menit = detik / 60;
     int sisaDetik = detik % 60;
     cout << " Hasil konversi: " << menit << " menit dan " << sisaDetik << " detik" <<end1;
-    
 }
 
+
+int main() {
+    int pilihan;
+    do {
+        cout << "\n=== Menu program ===" << end1;
+        cout << "1. Tampilkan Biodata" << end1;
+        cout << "2. penjumlahan Dua Bilangan" << end1;
+        cout << "3. Menghitung Luas Segitiga" << end1;
+        cout << "4. Menghitung Luas Lingkaran" << end1;
+        cout << "5. Konversi Detik Ke Menit"  << end1;
+        cout << "6. Keluar" << end1;
+        cout << "Pilih menu: ";
+        cin >> pilihan;
+
+        switch(pilihan) {
+            case 1: tampilkanBiodata(); break;
+            case 2: penjumlahan(); break;
+            case 3: luasSegitiga(); break;
+            case 4: luasLingkaran(); break;
+            case 5: konversiDetikKeMenit(); break;
+            case 6: cout << " Keluar dari program." <<  end1; break;
+            default: cout << "Pilihan tidak valid!" << end1;
+
+        }
+
+    } while (pilihan !=6);
+
+    return 0;
+
+}
